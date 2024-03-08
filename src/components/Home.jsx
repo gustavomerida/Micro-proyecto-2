3c2a27414e3ad7978075029eadeb55a19a7fb8d1
Gustavo <gmerida@correo.unimet.edu.ve>
2024-03-08 16:07:00 -0400

    Primer commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Página principal
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      Log in
+    </div>
+  )
+}
+
+export default Login
