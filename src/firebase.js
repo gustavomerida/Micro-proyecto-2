14b5afaf4ef299bbbd13fffd570c7c9a2265c081
Gustavo <gmerida@correo.unimet.edu.ve>
2024-03-08 18:34:08 -0400

    Firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsZ3bvYDgexgwm0eZQ5VxWBmaPy7OXp48",
+  authDomain: "micro-proyecto-2-f6cf2.firebaseapp.com",
+  projectId: "micro-proyecto-2-f6cf2",
+  storageBucket: "micro-proyecto-2-f6cf2.appspot.com",
+  messagingSenderId: "633131956840",
+  appId: "1:633131956840:web:a6a8dd33bea976fb3599dd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
